d3a4bfb429a3e72b7ff808938e6f6a03af981b96
DongJinLee <dlehdwls21@naver.com>
2023-09-21 17:20:53 +0900

    Feat : OW-46 CustomException 기능추가, ErrorCode 추가
    - CustomException ErrorMessage String 문자열으로 생성자에 입력하면 자동으로 변경되게 변경
    
    - EmailAuth 관련 ErrorCode 추가

--- back/src/main/java/com/ogjg/back/common/exception/CustomException.java
+++ back/src/main/java/com/ogjg/back/common/exception/CustomException.java
@@ -8,12 +8,25 @@ public abstract class CustomException extends RuntimeException {
     private final ErrorCode errorCode;
     private ErrorData errorData;
 
-    public CustomException(ErrorCode errorCode) {
+    public CustomException(
+            ErrorCode errorCode
+    ) {
         super(errorCode.getMessage());
         this.errorCode = errorCode;
     }
 
-    public CustomException(ErrorCode errorCode, ErrorData errorData) {
+    public CustomException(
+            ErrorCode errorCode,
+            String message
+    ) {
+        super(errorCode.changeMessage(message).getMessage());
+        this.errorCode = errorCode.changeMessage(message);
+    }
+
+    public CustomException(
+            ErrorCode errorCode,
+            ErrorData errorData
+    ) {
         super(errorCode.getMessage());
         this.errorCode = errorCode;
         this.errorData = errorData;
--- back/src/main/java/com/ogjg/back/common/exception/ErrorCode.java
+++ back/src/main/java/com/ogjg/back/common/exception/ErrorCode.java
@@ -9,21 +9,30 @@ public enum ErrorCode {
 
     SUCCESS(HttpStatus.OK, "200", "OK"),
     NOT_FOUND_USER(HttpStatus.NOT_FOUND, "404", "회원를 찾을 수 없습니다"),
-    INVALID_FORMAT(HttpStatus.BAD_REQUEST, "400", "데이터를 검증 실패");
+    INVALID_FORMAT(HttpStatus.BAD_REQUEST, "400", "데이터 검증 실패"),
+    AUTH_FAIL(HttpStatus.UNAUTHORIZED, "401", "인증에 실패 했습니다"),
+    EMAIL_AUTH_FAIL(HttpStatus.UNAUTHORIZED, "401", "이메일 인증에 실패 했습니다");
 
     @JsonIgnore
     private final HttpStatus statusCode;
     private final String code;
     private String message;
 
-    ErrorCode(HttpStatus statusCode, String code, String message) {
+    ErrorCode(
+            HttpStatus statusCode,
+            String code,
+            String message
+    ) {
         this.statusCode = statusCode;
         this.code = code;
         this.message = message;
     }
 
-    public ErrorCode changeMessage(String message) {
+    public ErrorCode changeMessage(
+            String message
+    ) {
         this.message = message;
         return this;
     }
+
 }
