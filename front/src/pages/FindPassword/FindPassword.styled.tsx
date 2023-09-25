25f717784c0ae34e3b9505f7384834088b584227
Sera Han <hsr0782@gmail.com>
2023-09-25 14:16:19 +0900

    Feat: OW-59 FE 비밀번호 찾기 페이지 UI
    비밀번호 찾기 기본 UI 구

---
+++ front/src/pages/FindPassword/FindPassword.styled.tsx
@@ -0,0 +1,116 @@
+import styled from "styled-components";
+import * as COLOR from "../../constants/color";
+import * as FONT from "../../constants/font";
+import { Link } from "react-router-dom";
+
+export const BackGround = styled.div`
+  display: flex;
+  width: 100%;
+  background-color: ${COLOR.Gray1};
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+`;
+
+export const FindPasswordForm = styled.div`
+  width: 340px;
+  height: 450px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const Title1 = styled.div`
+  font-size: ${FONT.Title};
+  font-weight: ${FONT.Medium};
+  text-align: center;
+  margin-bottom: 30px;
+`;
+
+export const Title2 = styled.div`
+  font-size: ${FONT.L};
+  text-align: center;
+  margin-bottom: 30px;
+`;
+
+export const Title3 = styled.div`
+  font-size: ${FONT.M};
+  text-align: center;
+  margin-bottom: 10px;
+`;
+
+export const StyledInputBox = styled.input`
+  width: 100%;
+  border: 1px solid ${COLOR.Gray2};
+  font-size: ${FONT.M};
+  outline: none;
+  line-height: 1em;
+  padding: 10px;
+  border-radius: 5px;
+  padding-right: 40px;
+  transition: 0.3s;
+  margin-bottom: 10px;
+
+  &::placeholder {
+    color: ${COLOR.Gray12};
+    font-size: ${FONT.S};
+  }
+
+  &:focus {
+    border: 1px solid ${COLOR.Purple1};
+  }
+  &::-ms-reveal,
+  ::-ms-clear {
+    display: none;
+  }
+`;
+
+export const StyledButton = styled.button`
+  background-color: ${COLOR.Purple1};
+  color: ${COLOR.White};
+  padding: 10px;
+  border: none;
+  border-radius: 5px;
+  font-size: ${FONT.M};
+  cursor: pointer;
+  width: 100%;
+  margin-top: 10px;
+
+  &:hover {
+    background-color: ${COLOR.Purple2};
+  }
+`;
+
+export const Error = styled.div`
+  font-size: ${FONT.M};
+  color: ${COLOR.Red1};
+  margin-bottom: 10px;
+`;
+
+export const LinkWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 10px;
+`;
+
+export const LoginLink = styled(Link)`
+  text-decoration: none;
+  color: ${COLOR.Purple1};
+  font-size: ${FONT.M};
+  text-align: center;
+  margin-top: 10px;
+`;
+
+export const HelpLink = styled(Link)`
+  text-decoration: none;
+  color: ${COLOR.Purple1};
+  font-size: ${FONT.M};
+  text-align: center;
+  margin-left: 10px;
+`;
+
+export const Helpinfo = styled.div`
+  font-size: ${FONT.M};
+  margin-left: 20px;
+`;
--- front/src/pages/FindPassword/FindPassword.tsx
+++ front/src/pages/FindPassword/FindPassword.tsx
@@ -1,5 +1,71 @@
-function FindPassword() {
-  return <div>FindPassword</div>;
-}
+import { useState } from "react";
+import * as S from "./FindPassword.styled";
+
+const FindPassword = () => {
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [isSent, setIsSent] = useState(false);
+  const [isEmailLocked, setIsEmailLocked] = useState(false);
+  const [showAlternateMessage, setShowAlternateMessage] = useState(false);
+
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
+  const handleFindPassword = async () => {
+    if (isEmailLocked) {
+      return; // 이메일 입력 잠금 상태
+    }
+    try {
+      setIsSent(true);
+      setMessage("비밀번호 재설정 메일을 보냈습니다. 이메일을 확인해주세요.");
+      setIsEmailLocked(true);
+      setShowAlternateMessage(true);
+    } catch (error) {
+      console.error(error);
+      setMessage("비밀번호 재설정 메일을 보내는 중 오류가 발생했습니다.");
+    }
+  };
+
+  return (
+    <S.BackGround>
+      <S.FindPasswordForm>
+        <S.Title1>OGJG</S.Title1>
+        <S.Title2>비밀번호 재설정</S.Title2>
+        <S.Title3>가입한 이메일 주소로 임시 비밀번호를 알려드립니다.</S.Title3>
+        <S.Title3>로그인 후 비밀번호를 꼭 변경해주세요.</S.Title3>
+
+        <S.StyledInputBox
+          type={"text"}
+          placeholder="이메일"
+          value={email}
+          onChange={handleEmailChange}
+          disabled={isEmailLocked}
+        />
+
+        {isSent ? (
+          <S.StyledButton>로그인</S.StyledButton>
+        ) : (
+          <S.StyledButton onClick={handleFindPassword}>인증 메일 전송</S.StyledButton>
+        )}
+
+        {message && <div>{message}</div>}
+        {isSent && (
+          <S.LinkWrapper>
+            {showAlternateMessage ? (
+              <S.LinkWrapper>
+                <S.Helpinfo>이미 계정이 있으세요?</S.Helpinfo>
+                <S.HelpLink to="/Login"> 로그인 </S.HelpLink>
+              </S.LinkWrapper>
+            ) : (
+              <S.LoginLink to="/Login"> 로그인으로 돌아가기 </S.LoginLink>
+            )}
+          </S.LinkWrapper>
+        )}
+        {!isSent && <S.LoginLink to="/Login"> 로그인으로 돌아가기 </S.LoginLink>}
+      </S.FindPasswordForm>
+    </S.BackGround>
+  );
+};
 
 export default FindPassword;
