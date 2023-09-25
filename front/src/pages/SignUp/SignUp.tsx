129403cee941d0555baa323a78583f90accf3fa1
Sera Han <hsr0782@gmail.com>
2023-09-25 10:57:22 +0900

    Feat : OW-57 FE 회원가입 페이지 UI
    회원가입 페이지 기본 UI 구현, 비밀번호 보기  아이콘 추가

--- front/src/pages/SignUp/SignUp.tsx
+++ front/src/pages/SignUp/SignUp.tsx
@@ -1,5 +1,48 @@
-function SignUp() {
-  return <div>SignUp</div>;
-}
+import { useState } from "react";
+import * as S from "./Signup.style";
+import Password from "./components/Password/Password";
 
-export default SignUp;
+const Signup = () => {
+  const [email, setEmail] = useState("");
+  const [isEmailSent, setIsEmailSent] = useState(false);
+
+  const handleSendEmailClick = () => {
+    setIsEmailSent(!isEmailSent);
+  };
+
+  const isButtonDisabled = email === "";
+
+  return (
+    <S.BackGround>
+      <S.SingupForm>
+        <S.Title1>OGJG</S.Title1>
+        <S.Title2>하나의 아이디로 OGJG 서비스를 이용하세요.</S.Title2>
+
+        <S.EmailInputWrapper>
+          <S.EmailInput
+            type="email"
+            value={email}
+            placeholder="이메일을 입력하세요"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <S.AuthButton onClick={handleSendEmailClick} disabled={isButtonDisabled}>
+            인증
+          </S.AuthButton>
+        </S.EmailInputWrapper>
+        {isEmailSent && <S.Error>인증 메일이 발송되었습니다.</S.Error>}
+
+        <Password />
+
+        <S.StyledInputBox type="text" id="username" placeholder="이름 (2~30자)" />
+        <S.StyledButton>회원가입</S.StyledButton>
+
+        <S.LinkWrapper>
+          <S.Logininfo>이미 계정이 있으세요?</S.Logininfo>
+          <S.LoginLink to="/Login"> 로그인 </S.LoginLink>
+        </S.LinkWrapper>
+      </S.SingupForm>
+    </S.BackGround>
+  );
+};
+
+export default Signup;
---
+++ front/src/pages/SignUp/Signup.style.tsx
@@ -0,0 +1,125 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import * as COLOR from "../../constants/color";
+import * as FONT from "../../constants/font";
+
+export const BackGround = styled.div`
+  display: flex;
+  width: 100%;
+  background-color: ${COLOR.Gray1};
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+`;
+
+export const SingupForm = styled.div`
+  width: 340px;
+  height: 450px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  line-height: 27px;
+`;
+
+export const Title1 = styled.div`
+  font-size: ${FONT.Title};
+  font-weight: ${FONT.Medium};
+  margin: 10px;
+  text-align: center;
+`;
+
+export const Title2 = styled.div`
+  font-size: ${FONT.M};
+  margin: 10px;
+  line-height: 80px;
+  text-align: center;
+`;
+
+export const EmailInputWrapper = styled.div`
+  position: relative;
+`;
+
+export const StyledInputBox = styled.input`
+  width: 100%;
+  border: 1px solid ${COLOR.Gray2};
+  font-size: ${FONT.M};
+  outline: none;
+  line-height: 1em;
+  padding: 10px;
+  border-radius: 5px;
+  padding-right: 40px;
+  transition: 0.3s;
+  margin-bottom: 10px;
+
+  &::placeholder {
+    color: ${COLOR.Gray12};
+    font-size: ${FONT.S};
+  }
+
+  &:focus {
+    border: 1px solid ${COLOR.Purple1};
+  }
+  &::-ms-reveal,
+  ::-ms-clear {
+    display: none;
+  }
+`;
+
+export const EmailInput = styled(StyledInputBox)`
+  width: 340px;
+`;
+
+export const AuthButton = styled.button`
+  position: absolute;
+  top: 40%;
+  right: 10px;
+  transform: translateY(-50%);
+  background-color: ${(props) => (props.disabled ? "#ccc" : "#525BFF")};
+  padding: 5px 10px;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: ${(props) => (props.disabled ? "not-allowed" : "pointer")};
+`;
+
+export const Error = styled.div`
+  font-size: ${FONT.M};
+  color: ${COLOR.Red1};
+  margin-bottom: 10px;
+`;
+
+export const StyledButton = styled.button`
+  background-color: ${COLOR.Purple1};
+  color: ${COLOR.White};
+  padding: 10px;
+  border: none;
+  border-radius: 5px;
+  font-size: ${FONT.M};
+  cursor: pointer;
+  width: 100%;
+  margin-top: 10px;
+
+  &:hover {
+    background-color: ${COLOR.Purple2};
+  }
+`;
+
+export const LinkWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 10px;
+`;
+
+export const LoginLink = styled(Link)`
+  text-decoration: none;
+  color: ${COLOR.Purple1};
+  font-size: ${FONT.M};
+  text-align: center;
+  margin-left: 10px;
+`;
+
+export const Logininfo = styled.div`
+  font-size: ${FONT.M};
+  margin-left: 20px;
+`;
