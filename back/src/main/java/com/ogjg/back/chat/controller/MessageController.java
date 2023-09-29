748f086e720bb67366eea8c2caf28974b4d186b3
ByeongGyu An <devafox62@gmail.com>
2023-09-30 04:39:35 +0900

    Feat : OW-77 실시간 채팅 연결, 통신 로직 및 API 구현
    채팅방 구독 로직
    채팅방 최초 입장 시 해당 채팅방에 있는 유저에게 입장 메세지를 전송하도록 구현. 현재 유저의 정보를 SessionAttribute에 저장하고, 실제 DB에도 메세지를 저장
    
    채팅방 메시지 전송
    참여중인 채팅방에 메시지를 전송하도록 구현
    
    채팅방 유저 리스트 조회
    현재 채팅방에 참여 중인 유저들을 반환.
    
    채팅방 퇴장
    웹소켓의 연결이 끊어지면 작동하는 @EventListener를 사용하여 구현. 구독 시 저장했던 SessionAttribute의 내부 데이터로 식별하여 메시지를 전송하도록 작성

---
+++ back/src/main/java/com/ogjg/back/chat/controller/MessageController.java
@@ -0,0 +1,60 @@
+package com.ogjg.back.chat.controller;
+
+import com.ogjg.back.chat.domain.MessageType;
+import com.ogjg.back.chat.dto.ChattingRoomUserInfoDto;
+import com.ogjg.back.chat.dto.MessageDto;
+import com.ogjg.back.chat.service.MessageService;
+import com.ogjg.back.common.exception.ErrorCode;
+import com.ogjg.back.common.response.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/chat")
+@Slf4j
+public class MessageController {
+
+    private final MessageService messageService;
+
+    @MessageMapping("/chat/enter-room")
+    public void enterRoom(@Payload MessageDto message, SimpMessageHeaderAccessor headerAccessor) {
+
+        messageService.addUserInfoInSessionAttribute(message, headerAccessor);
+        messageService.saveUserRoom(message);
+
+        message.setType(MessageType.ENTER);
+        message.setContent(message.getSender() + "님이 입장하셨습니다.");
+
+        messageService.saveAndSendMessage(message);
+    }
+
+    @MessageMapping("/chat/send-message")
+    public void sendMessage(@Payload MessageDto message) {
+        messageService.saveAndSendMessage(message);
+    }
+
+    @GetMapping("/{containerId}/users")
+    public ApiResponse<?> getUsersInChattingRoom(@PathVariable Long containerId) {
+
+        List<ChattingRoomUserInfoDto> users = messageService.getUsersInChattingRoom(containerId);
+        return new ApiResponse<>(ErrorCode.SUCCESS, users);
+    }
+
+    @GetMapping("/{containerId}/messages")
+    public ApiResponse<?> getMessagesInChattingRoom(@PathVariable Long containerId) {
+
+        List<MessageDto> responseData = messageService.getMessagesInChattingRoom(containerId);
+        return new ApiResponse<>(ErrorCode.SUCCESS, responseData);
+    }
+}
