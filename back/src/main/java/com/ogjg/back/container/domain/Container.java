524d437147b2d9aa47d58e4c4138a3eee73d7b27
ByeongGyu An <devafox62@gmail.com>
2023-10-04 14:07:32 +0900

    Fix : Container 엔티티 정규식 위치 수정
    잘못된 위치에 있던 정규식 옵션을 수정

--- back/src/main/java/com/ogjg/back/container/domain/Container.java
+++ back/src/main/java/com/ogjg/back/container/domain/Container.java
@@ -26,12 +26,12 @@ public class Container {
     @JoinColumn(name = "email")
     private User user;
 
-    @Pattern(regexp = "^[a-zA-Z0-9\\-_]{1,20}$",
-            message = "컨테이너 이름에는 영문, 숫자가 포함가능하며, 특수문자는 '-', '_'만 포함될 수 있습니다.")
     @OneToOne
     @JoinColumn(name = "room_id")
     private Room room;
 
+    @Pattern(regexp = "^[a-zA-Z0-9\\-_]{1,20}$",
+            message = "컨테이너 이름에는 영문, 숫자가 포함가능하며, 특수문자는 '-', '_'만 포함될 수 있습니다.")
     private String name;
 
     private String description;
