1a680214f392d58598cc5772828fea4ce7c6d601
ByeongGyu An <devafox62@gmail.com>
2023-10-04 17:44:04 +0900

    Feat : OW-91 UnauthorizedUserAccessException 정의
    인가되지 않은 사용자가 접근할 때 발생시키기 위한 예외 작성

--- back/src/main/java/com/ogjg/back/common/exception/ErrorCode.java
+++ back/src/main/java/com/ogjg/back/common/exception/ErrorCode.java
@@ -9,6 +9,7 @@ public enum ErrorCode {
 
     SUCCESS(HttpStatus.OK, "200", "OK"),
     LOGIN_FAIL(HttpStatus.UNAUTHORIZED, "401", "아이디와 비밀번호를 확인 해주세요"),
+    UNAUTHORIZED_USER_ACCESS(HttpStatus.UNAUTHORIZED, "401", "인가되지 않은 유저의 접근입니다."),
     FAIL_SIGNUP(HttpStatus.BAD_REQUEST, "400", "회원가입에 실패 했습니다"),
     NOT_FOUND_USER(HttpStatus.NOT_FOUND, "404", "회원를 찾을 수 없습니다"),
     NOT_FOUND_CONTAINER(HttpStatus.NOT_FOUND, "400", "컨테이너를 찾을 수 없습니다"),
