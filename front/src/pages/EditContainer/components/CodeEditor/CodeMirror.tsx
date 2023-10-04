b47dc0b3b177da61bb97d0bcf514230916384212
Narcoker <jykim9868@gmail.com>
2023-10-04 14:17:20 +0900

    Feat : OW-86 파일별 CRDT 구현 완료

--- front/src/pages/EditContainer/components/CodeEditor/CodeMirror.tsx
+++ front/src/pages/EditContainer/components/CodeEditor/CodeMirror.tsx
@@ -14,6 +14,8 @@ import { javascript } from "@codemirror/lang-javascript";
 import { json } from "@codemirror/lang-json";
 import { java } from "@codemirror/lang-java";
 import { python } from "@codemirror/lang-python";
+import yorkie, { OperationInfo } from "yorkie-js-sdk";
+import { YorkieDoc } from "../../../../types/yorkieType";
 
 function CodeMirror() {
   const [tabs, setTabs] = useRecoilState(tabsState);
@@ -26,9 +28,9 @@ function CodeMirror() {
 
   const getLangExtensions = (extension: string) => {
     if (extension === "js") return javascript();
-    if (extension === "jsx") return javascript();
-    if (extension === "ts") return javascript();
-    if (extension === "tsx") return javascript();
+    if (extension === "jsx") return javascript({ jsx: true });
+    if (extension === "ts") return javascript({ typescript: true });
+    if (extension === "tsx") return javascript({ jsx: true, typescript: true });
     if (extension === "css") return css();
     if (extension === "json") return json();
     if (extension === "html") return html();
@@ -55,38 +57,125 @@ function CodeMirror() {
   }, [saveActiveTabFile]);
 
   useEffect(() => {
-    if (editorRef.current && !cmViewRef.current) {
-      const el = editorRef.current;
+    let cmView: EditorView;
+    const initializeEditor = async () => {
+      if (editorRef.current && !cmViewRef.current) {
+        // yorkie setting
+        const client = new yorkie.Client("https://api.yorkie.dev", {
+          apiKey: "cedaovjuioqlk4pjqn6g",
+        });
+        await client.activate();
 
-      const activeTabExtension = tabs.files[tabs.active].split(".").pop() as string;
-      console.log("activeTabExtension", activeTabExtension);
-      const langExtension = getLangExtensions(activeTabExtension);
+        // 02-1. create a document then attach it into the client.
+        const doc = new yorkie.Document<YorkieDoc>(
+          `narcoker${tabs.files[tabs.active].replace(/\//g, "-")}`, //          narcoker-root-duck-duck1.css
+        );
 
-      const cmView = new EditorView({
-        doc: tabs.codes[tabs.active],
-        extensions: [
-          basicSetup,
-          vscodeDark,
-          ...(langExtension ? [langExtension] : []),
-          autocompletion(),
-          keymap.of([
-            { key: isMac ? "Cmd-Space" : "Ctrl-Space", run: startCompletion },
-            ...searchKeymap,
-          ]),
-          EditorState.transactionFilter.of((transaction) => {
-            if (transaction.docChanged) {
-              handleCode(transaction);
+        await client.attach(doc);
+
+        doc.update((root) => {
+          if (!root.content) {
+            root.content = new yorkie.Text();
+          }
+        }, "create content if not exists");
+
+        // 02-2. subscribe document event.
+        const syncText = () => {
+          const text = doc.getRoot().content;
+          cmView.dispatch({
+            changes: { from: 0, to: cmView.state.doc.length, insert: text.toString() },
+            annotations: [Transaction.remote.of(true)],
+          });
+        };
+
+        doc.subscribe("$.content", (event) => {
+          if (event.type === "remote-change") {
+            const { operations } = event.value;
+            handleOperations(operations);
+          }
+        });
+
+        await client.sync();
+
+        // 03-1. define function that bind the document with the codemirror(broadcast local changes to peers)
+        const updateListener = EditorView.updateListener.of((viewUpdate) => {
+          if (viewUpdate.docChanged) {
+            for (const tr of viewUpdate.transactions) {
+              const events = ["select", "input", "delete", "move", "undo", "redo"];
+              if (!events.map((event) => tr.isUserEvent(event)).some(Boolean)) {
+                continue;
+              }
+              if (tr.annotation(Transaction.remote)) {
+                continue;
+              }
+              tr.changes.iterChanges((fromA, toA, _, __, inserted) => {
+                doc.update((root) => {
+                  root.content.edit(fromA, toA, inserted.toJSON().join("\n"));
+                }, `update content byA ${client.getID()}`);
+              });
             }
-            return transaction;
-          }),
-        ],
-      });
+          }
+        });
 
-      cmView.dom.style.height = "100%";
-      el.innerHTML = "";
-      el.appendChild(cmView.dom);
+        // 03-2. create codemirror instance
+        const el = editorRef.current;
 
-      //shortcut: zoom in out
+        const activeTabExtension = tabs.files[tabs.active].split(".").pop() as string;
+        const langExtension = getLangExtensions(activeTabExtension);
+
+        cmView = new EditorView({
+          doc: tabs.codes[tabs.active],
+          extensions: [
+            basicSetup,
+            vscodeDark,
+            ...(langExtension ? [langExtension] : []),
+            autocompletion(),
+            keymap.of([
+              { key: isMac ? "Cmd-Space" : "Ctrl-Space", run: startCompletion },
+              ...searchKeymap,
+            ]),
+            EditorState.transactionFilter.of((transaction) => {
+              if (transaction.docChanged) {
+                handleCode(transaction);
+              }
+              return transaction;
+            }),
+            updateListener,
+          ],
+        });
+
+        cmView.dom.style.height = "100%";
+        el.innerHTML = "";
+        el.appendChild(cmView.dom);
+
+        // 03-3. define event handler that apply remote changes to local
+        const handleOperations = (operations: Array<OperationInfo>) => {
+          operations.forEach((op) => {
+            if (op.type === "edit") {
+              handleEditOp(op);
+            }
+          });
+        };
+        const handleEditOp = (op: any) => {
+          const changes = [
+            {
+              from: Math.max(0, op.from),
+              to: Math.max(0, op.to),
+              insert: op.value!.content,
+            },
+          ];
+
+          cmView.dispatch({
+            changes,
+            annotations: [Transaction.remote.of(true)],
+          });
+        };
+
+        syncText();
+      }
+    };
+
+    initializeEditor().then(() => {
       cmView.dom.style.fontSize = `${fontSize}px`;
 
       const handleZoom = (event: WheelEvent) => {
@@ -116,7 +205,8 @@ function CodeMirror() {
         cmView.dom.removeEventListener("wheel", handleZoom);
         cmView.dom.removeEventListener("keydown", handleSave);
       };
-    }
+    });
+    //shortcut: zoom in out
   }, [tabs.codes.length, tabs.active, fontSize]);
 
   return (
