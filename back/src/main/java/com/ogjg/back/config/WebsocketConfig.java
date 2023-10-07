4b1b3c7da52066fc6cca109a16e5970a8db24d39
ByeongGyu An <devafox62@gmail.com>
2023-10-08 00:25:28 +0900

    Fix : Origin을 환경변수로 적용
    prod, dev 서버의 도메인을 환경 변수로 적용하여 등록

--- back/src/main/java/com/ogjg/back/config/WebsocketConfig.java
+++ back/src/main/java/com/ogjg/back/config/WebsocketConfig.java
@@ -1,5 +1,6 @@
 package com.ogjg.back.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -10,6 +11,12 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 public class WebsocketConfig implements WebSocketMessageBrokerConfigurer {
 
+    @Value("${origins.prod}")
+    private String prodDomain;
+
+    @Value("${origins.dev}")
+    private String devDomain;
+
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/sub");
@@ -19,7 +26,7 @@ public class WebsocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws/chat")
-                .setAllowedOrigins("https://ogjg.site")
+                .setAllowedOrigins(prodDomain, devDomain)
                 .withSockJS();
     }
 }
